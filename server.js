7dab137633a62c86ceb5f3efe07c3e65d52c03a7
Gabriel Geisler <gabriel.fgeisler@gmail.com>
2026-08-21 23:52:46 -0300

    initial commit

--- server.js
+++ server.js
@@ -71,6 +71,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
